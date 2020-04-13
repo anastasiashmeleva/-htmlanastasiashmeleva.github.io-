6e40cdc63499de17cf7b69f59e52053e3def1778
horuzma <62601538+horuzma@users.noreply.github.com>
2020-04-13 22:27:40 +0300

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -14,67 +14,46 @@ imgs.forEach(function(item) {
 });
 */
 
-let imgs = document.querySelectorAll('.img');
-imgs.forEach( function( item ) {
-	let img = item.querySelector('img'); img.classList.toggle('hide');
-	
-	img.onload = function( ){
-		let orig = document.createElement('div');  orig.className = "orig";
-		let wrap_o = document.createElement('div'); wrap_o.className = "wrap";
-		let orig_img = document.createElement('img');  orig_img.src = img.src;
-		wrap_o.append(orig_img); orig.append(wrap_o); item.append(orig);
-		
-		let clone = document.createElement('div'); clone.className = "clone";
-		let wrap_c = document.createElement('div'); wrap_c.className = "wrap";
-		let clone_img = document.createElement('img'); clone_img.src = img.src;
-		wrap_c.append(clone_img); clone.append(wrap_c); item.append(clone);
-
-		setTimeout(function(){
-			item.style._padding = ( item.querySelector('.wrap').clientHeight / 2 / 2 ) + "px";
-		},100);
-	}
+	var wow = new WOW();
+	wow.init();
 	
 	
-	//innerHTML
-//						<div class="orig"><img title="BLAH BLAH BLAH" alt="BLAH BLAH BLAH" border="0" src="assets/cover.jpg" /></div>
-//					<div class="clone"><img title="BLAH BLAH BLAH" alt="BLAH BLAH BLAH" border="0" src="assets/cover.jpg" /></div>
+		let imgs = document.querySelectorAll('.img');
+		imgs.forEach( function( item ) {
+			let img = item.querySelector('img');
+			img.classList.toggle('hide');
+			//img.onload = function( ){
+				let orig = document.createElement('div');  orig.className = "orig";
+				let wrap_o = document.createElement('div'); wrap_o.className = "wrap";
+				let orig_img = document.createElement('img');  orig_img.src = img.src;
+				wrap_o.append(orig_img); orig.append(wrap_o); item.append(orig);
+				
+				let clone = document.createElement('div'); clone.className = "clone";
+				let wrap_c = document.createElement('div'); wrap_c.className = "wrap";
+				let clone_img = document.createElement('img'); clone_img.src = img.src;
+				wrap_c.append(clone_img); clone.append(wrap_c); item.append(clone);
 
-	item.addEventListener('click', function() {
-		this.classList.toggle('mirror');
-		if ( this.classList.contains("mirror") ){
-			this.querySelector(".orig").style.paddingTop = this.style._padding;
-			this.querySelector(".orig").style.paddingBottom = this.style._padding;
-			this.querySelector(".clone").style.paddingTop = this.style._padding;
-			this.querySelector(".clone").style.paddingBottom = this.style._padding;
-		} else {
-			this.querySelector(".orig").style.paddingTop = 0;
-			this.querySelector(".orig").style.paddingBottom = 0;
-			this.querySelector(".clone").style.paddingTop = 0;
-			this.querySelector(".clone").style.paddingBottom = 0;
-		}
-	});  
-
-
-
-
-var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       0,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null // optional scroll container selector, otherwise use window
-  }
-);
-wow.init();
+				setTimeout(function(){
+					item.style._padding = ( item.querySelector('.wrap').clientHeight / 2 / 2 ) + "px";
+				},100);
+			//}
+			item.addEventListener('click', function() {
+				this.classList.toggle('mirror');
+				if ( this.classList.contains("mirror") ){
+					this.querySelector(".orig").style.paddingTop = this.style._padding;
+					this.querySelector(".orig").style.paddingBottom = this.style._padding;
+					this.querySelector(".clone").style.paddingTop = this.style._padding;
+					this.querySelector(".clone").style.paddingBottom = this.style._padding;
+				} else {
+					this.querySelector(".orig").style.paddingTop = 0;
+					this.querySelector(".orig").style.paddingBottom = 0;
+					this.querySelector(".clone").style.paddingTop = 0;
+					this.querySelector(".clone").style.paddingBottom = 0;
+				}
+			});
+		});
 
 
-});
 
 
 
